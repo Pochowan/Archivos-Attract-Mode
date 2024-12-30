104c78b51a1a3994a60d78092ff1d38aa372366f
Pochowan <54528676+Pochowan@users.noreply.github.com>
2024-12-30 20:28:41 +0100

    Add files via upload

---
+++ Reinicio-script.bat
@@ -0,0 +1,21 @@
+@Echo off
+if not "%1" == "max" start /MAX cmd /c %0 max & exit/b
+
+title Actualizando script,espera...
+echo.
+
+echo Descargando,espera...
+
+powershell -command "& {iwr https://github.com/Pochowan/Instalador-Attract-Mode-Pack-Base-738Gb/raw/refs/heads/main/Paquete-update-3.rar -OutFile Paquete-update-3.rar}"
+
+"C:\Program Files\7-Zip\7z.exe" x Paquete-update-3.rar -y > NUL
+
+timeout /t 10
+
+start "" "Configurar Lightguns.exe"
+
+msg * Actualizado.
+
+exit
+
+
