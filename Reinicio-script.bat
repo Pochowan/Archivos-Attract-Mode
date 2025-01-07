06da2c716574790361db80acc1e118f40d7cbdba
Pochowan <54528676+Pochowan@users.noreply.github.com>
2025-01-07 22:52:22 +0100

    Add files via upload

--- Reinicio-script.bat
+++ Reinicio-script.bat
@@ -4,20 +4,25 @@ if not "%1" == "max" start /MAX cmd /c %0 max & exit/b
 title Actualizando script,espera...
 echo.
 
+reg add "HKCU\Control Panel\Accessibility\StickyKeys" /v Flags /t REG_DWORD /d 0 /f
+reg add "HKCU\Control Panel\Accessibility\StickyKeys" /v HotkeyOnly /t REG_DWORD /d 0 /f
+reg add "HKCU\Control Panel\Accessibility\StickyKeys" /v InitialToggle /t REG_DWORD /d 0 /f
+reg add "HKCU\Control Panel\Accessibility\StickyKeys" /v Sound /t REG_DWORD /d 0 /f
+
 echo Descargando,espera...
 echo.
 powershell -command "& {iwr https://github.com/Pochowan/Instalador-Attract-Mode-Pack-Base-738Gb/raw/refs/heads/main/Paquete-update-3.rar -OutFile Paquete-update-3.rar}"
-echo.
+
 cls
-echo.
+
 echo Descomprimiendo,espera...
 
 "C:\Program Files\7-Zip\7z.exe" x Paquete-update-3.rar -y > NUL
-echo.
+
 cls
-echo.
+
 echo Instalado,el script para configurar Lightguns iniciara en 10 segundos...
-echo.
+
 timeout /t 10
 
 powershell -command Remove-Item -recurse 'Paquete-update-3.rar' -erroraction 'silentlycontinue'
