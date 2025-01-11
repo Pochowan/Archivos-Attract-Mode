4927c844844b6d94425ffacc08057623b07b80e0
Pochowan <54528676+Pochowan@users.noreply.github.com>
2025-01-12 00:07:40 +0100

    Add files via upload

--- Reinicio-Lightguners-script.bat
+++ Reinicio-Lightguners-script.bat
@@ -32,7 +32,11 @@ start "" "Configurar Lightguns.exe"
 msg * Modo Arcade The House Of The Dead 3 instalado.
 msg * Teclas especiales Rshift desactivadas (no se pulsara accidentalmente)
 msg * Bezels Sega Model 2 corregidos.
-msg * Actualizado script (7 de Enero 2025)
+msg * (Super Nintendo) Lethal Enforcers ahora se puede jugar a 2 jugadores.
+msg * (Super Nintendo) T2 Arcade ahora se puede jugar a 2 jugadores (mira acelerada)
+msg * (Super Nintendo) Operation Thunderbolt modo para 2 jugadores,tiene un bug al iniciar,mira paralizada.
+msg * Nuevas opciones disponibles para añadir contenido extra (Activar/desactivar mira ,cabmio de formato,todo dentro de AttracT Mode)
+msg * Actualizado script (12 de Enero 2025)
 
 exit
 
