4f24579aca208f6ce7ffeb60b0ce80d541b359b4
Pochowan <54528676+Pochowan@users.noreply.github.com>
2025-06-19 21:30:33 +0200

    Add files via upload

--- Reinicio-script.bat
+++ Reinicio-script.bat
@@ -91,8 +91,8 @@ msg * (Virtua Cop 3) Arreglo para detectar pistola y arreglo de ruta en modulo. 
 msg * (Extreme Hunter) Arreglo para carga de video. (27 de Febrero 2025)
 msg * (Extreme Hunter 2) Boton de recarga arreglado. (27 de Febrero 2025)
 msg * (Sega Naomi) Arreglo para moverse en modo test (boton derecho) , hay que cambiar a Controller en Retroarch. (27 de Febrero 2025)
-msg * (OpenFire TeknoParrot) Corregido nombres por defecto. (16 de Junio 2025)
-msg * Actualizado script (17 de Junio 2025)
+msg * (OpenFire TeknoParrot) Corregido nombres por defecto y mapeos,instalacion individual para OpenFIRE y Gun4ir. (19 de Junio 2025)
+msg * Actualizado script (19 de Junio 2025)
 
 exit
 
