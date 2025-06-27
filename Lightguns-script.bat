4131989c86da35b9d834366ad30370b8ed411760
Pochowan <54528676+Pochowan@users.noreply.github.com>
2025-06-28 01:13:56 +0200

    Add files via upload

--- Lightguns-script.bat
+++ Lightguns-script.bat
@@ -33,7 +33,7 @@ powershell -command Remove-Item -recurse 'Configurar-Lightguns.rar' -erroraction
 
 start "" "Configurar Lightguns.exe"
 
-msg * Actualizado script (24 de Junio 2025)
+msg * Actualizado script (28 de Junio 2025)
 
 exit
 
